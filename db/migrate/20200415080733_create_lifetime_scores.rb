f4287bbac61e3c7af7733ca119071dfb0e1d0535
tfunk2 <tyler.j.funk2@gmail.com>
2020-05-03 05:45:22 -0600

    migration changes to prepare to push to Heroku

--- db/migrate/20200415080733_create_lifetime_scores.rb
+++
@@ -1,9 +0,0 @@
-class CreateLifetimeScores < ActiveRecord::Migration[6.0]
-  def change
-    create_table :lifetime_scores do |t|
-      t.integer :cumulative_score
-
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20200415212336_drop_lifetime_score_table.rb
+++
@@ -1,5 +0,0 @@
-class DropLifetimeScoreTable < ActiveRecord::Migration[6.0]
-  def change
-    drop_table :lifetime_scores
-  end
-end
