f2b5c81c023fa30cb0e1589de3dc75c98932e606
tfunk2 <tyler.j.funk2@gmail.com>
2020-05-03 18:46:45 -0600

    change cors file to accept only from front end

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'https://brain-spark.firebaseapp.com' 
 
     resource '*',
       headers: :any,
